89100eacb6f5793181c72826548da87366502f3d
Kyo <darren.temple.101@gmail.com>
2017-04-30 23:22:42 -0700

    Reading Blackboard Data In C++ - Enabled an "IndexKey" variable in the new "ChooseNextWaypoint" C++ class, which can then be accessed in the UE "Patrol" behaviour tree - Found the first waypoint index

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,10 +2,13 @@
 
 #include "TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("JumJum"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
